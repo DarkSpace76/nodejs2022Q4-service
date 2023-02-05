95a38b319d61a8ff217f018a06fe65a4dfda5103
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2023-02-05 21:44:23 +0300

    feat: add favoriters class extends

---
+++ src/utils/favs.helper.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export default class FavoritesHelper {
+    code: number;
+    message: string;
+
+    constructor(code: number, message: string) {
+        this.code = code;
+        this.message = message;
+    }
+}
